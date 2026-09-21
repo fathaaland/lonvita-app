e3413e7d5df0ac1cb11ffbaa3c7f82565b52f44b
David Dobrovolny <dejv.dobrovolny642001@gmail.com>
2026-09-21 11:29:47 +0200

    update city description

--- src/lib/seed/run.ts
+++ src/lib/seed/run.ts
@@ -79,7 +79,7 @@ const DEMO_PASSWORD = 'demo1234'
 const MUNICIPALITIES = [
   {
     name: 'Nové Veselí',
-    description: 'Nové Veselí na Žďársk a okolí a na Vysočině',
+    description: 'Nové Veselí na Žďársk a okolí a na Vysočině a Česku',
     lat: 49.5198752,
     lng: 15.9084979,
   },
